86ff9234238c823f59ab96744c4451d5be4d3d24
fay-wu <mfaywu@ucla.edu>
2014-04-15 02:24:35 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<title>I like crab.</title>
+<header>
+	<h1>I suppose I should have my own website, <br>since I can't find the one I made in third grade.</h1>
+</header>
+<body>
+	<p>
+		My name is Fay<br>
+		I go to ucla<br>
+		I like crab.<br>
+	</p>
+	<p>
+		Email me at <a href="mailto:mfaywu@ucla.edu">mfaywu@ucla.edu</a>
+	</p>
+</body>
+</html>
