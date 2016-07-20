264e9405aaaa00f608afce4108dfdcee77f82bb3
M. Fay Wu <mfaywu@ucla.edu>
2016-07-20 07:24:01 -0700

    Remove pyc files and add contact page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
 	return render(request, 'home/index.html')
+
+def contact(request):
+	return render(request, 'home/basic.html',{'content':['If you would like to contact me, please email me.','mfaywu@ucla.edu']})
