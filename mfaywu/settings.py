89e741ad300e80584adea9b7ab2fa8ed8160350a
M. Fay Wu <mfaywu@ucla.edu>
2016-07-20 09:13:31 -0700

    Change secret key.

--- mfaywu/settings.py
+++ mfaywu/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_&p(2a-$no+lp0m=54i33pzhzr4+wg6jsl+3uw#!nr!52*@ys^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
