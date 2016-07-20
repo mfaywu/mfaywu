255fcbd85fd383df0938f6b082a9796ab38c6ba5
M. Fay Wu <mfaywu@ucla.edu>
2016-07-20 08:37:03 -0700

    Add post view

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=140)
+    body = models.TextField()
+    image = models.ImageField(blank=True,null=True)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.title
